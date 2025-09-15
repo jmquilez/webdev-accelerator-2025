f48df61f13718a6c34cc4d5c21300fc478525a6d
jmquilez <finalizer2869@gmail.com>
2025-09-15 16:13:41 +0200

    added adapter to build & run in prod

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,21 @@
 // @ts-check
 import { defineConfig, envField } from 'astro/config';
+import node from '@astrojs/node';
 
 // https://astro.build/config
 export default defineConfig({
   output: 'server',
+  adapter: node({
+    mode: 'standalone'
+  }),
   env: {
-    schema: {N2YO_API_KEY: envField.string({context: "client",
-      access: "public",
-      optional: true,
-      default: "pito"})
+    schema: {
+      N2YO_API_KEY: envField.string({
+        context: "client",
+        access: "public",
+        optional: true,
+        default: "DEMO_KEY"
+      })
     }
   }
 });
